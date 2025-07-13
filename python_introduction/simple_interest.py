004e67fc15ae93b67db1e31f7e3dd46e20d4a095
Onditi9 <omwegajoseph01@gmail.com>
2025-07-13 20:53:05 +0300

    simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
 #calculating simple interest
 
-pricipai = 1000
+principal = 1000
 rate = 0.05
 time = 3
 
-interest = pricipai * rate * time
+interest = principal * rate * time
 
 print ("The simple interest is:", interest)
