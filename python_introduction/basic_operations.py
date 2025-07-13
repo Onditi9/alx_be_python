cdcc91ae07c0e2c59e9f7cfba2877b9ef9f1a083
Onditi9 <omwegajoseph01@gmail.com>
2025-07-13 20:01:35 +0300

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10  
+number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print("Addition of 10 and 5 is", number1 + number2)
+print("Subtraction of 10 and 5 is", number1 - number2)
+print("Multiplication of 10 and 5 is", number1 * number2)
