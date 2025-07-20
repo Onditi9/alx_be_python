0cf952c764809faa8a6d514053ee5943f06a90e2
Onditi9 <omwegajoseph01@gmail.com>
2025-07-20 23:51:35 +0300

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,31 @@
-# daily_reminder.py
+# Personal Daily Reminder
 
-task = input("What's your task for today? ")
-priority = input("Priority level (high, medium, low): ").lower()
-time_bound = input("Is the task time-bound? (yes/no): ").lower()
+# Ask the user to input a task description,task priority and time bound option
+task = input("Enter your task: ")
+print()
+priority = input("Priority (high/medium/low): ")
+print()
+time_bound =input("Is it time-bound? (yes/no): ")
 
+# Match Case statements that react differently based on the task’s priority
 match priority:
     case "high":
-        print(f"High-priority alert! Don't forget to: {task}.")
-        if time_bound == "yes":
-            print("This is time-sensitive! Schedule it ASAP.")
-        else:
-            print("You’ve got time, but still—it’s important!")
+      if time_bound == "yes":
+        print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
+    
+      else:
+        print(f"Note:{task} is a high priority task. Consider completing it before the day ends.")
+
     case "medium":
-        print(f"Medium-priority reminder: {task}.")
-        if time_bound == "yes":
-            print(" It has a deadline, so plan accordingly.")
-        else:
-            print("Take it step-by-step. You’ve got this!")
+      if time_bound == "yes":
+        print(f"{task} is a medium priority task that should be atttended as soon possible!")
+    
+      else:
+        print(f"{task} is a medium priority task. Consider doing it when you are less busy .")
+
     case "low":
-        print(f"Low-priority note: {task}.")
-        if time_bound == "yes":
-            print("It’s time-bound, even if it’s not urgent.")
-        else:
-            print("Good to have on the radar. No pressure.")
-    case _:
-        print(" I didn’t catch that priority level. Try again with high, medium, or low.")
+      if time_bound == "yes":
+        print(f"{task} is a low priority task that can be completed within the week")
+    
+      else:
+        print(f"{task} is a low priority task. Consider completing it when you have free time.")
