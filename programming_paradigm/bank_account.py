f93f6e64a9e1647820af5c3453742f4dcfc25cfb
Onditi9 <omwegajoseph01@gmail.com>
2025-07-30 16:07:10 +0300

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,12 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+    def _init_(self, balance=0):
+        self.balance = balance
 
     def deposit(self, amount):
-        self.account_balance += amount
+        self.balance += amount
 
     def withdraw(self, amount):
-        if amount <= self.account_balance:
-            self.account_balance -= amount
-            return True
-        else:
-            return False
+        self.balance -= amount
 
     def display_balance(self):
-        print(f"Current account balance: ${self.account_balance:.2f}")
+        print("Current Balance:", self.balance)
