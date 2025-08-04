4b6aae49435f67d9312a9b81d92e0fcc8915dec1
Onditi9 <omwegajoseph01@gmail.com>
2025-08-05 00:24:56 +0300

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,25 +1,17 @@
-# library_system.py
+# book_class.py
 
 class Book:
-    def _init_(self, title, author):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
+        self.year = year
+        print(f"Book created: {self}")
 
-    def _str_(self):
-        return f"{self.title} by {self.author}"
+    def __del__(self):
+        print(f"Deleting {self.title}")
 
-class EBook(Book):
-    def _init_(self, title, author, file_size):
-        super()._init_(title, author)
-        self.file_size = file_size
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
 
-    def _str_(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}MB"
-
-class PrintBook(Book):
-    def _init_(self, title, author, pages):
-        super()._init_(title, author)
-        self.pages = pages
-
-    def _str_(self):
-        return f"PrintBook: {self.title} by {self.author}, Pages: {self.pages}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
