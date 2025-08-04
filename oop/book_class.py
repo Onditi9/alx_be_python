b29fbc9a2c4a42100b98d6ede9eb49e89d6b9bd8
Onditi9 <omwegajoseph01@gmail.com>
2025-08-05 00:15:42 +0300

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,20 +1,25 @@
+# library_system.py
+
 class Book:
-    def _init_(self, title, author, year):
+    def _init_(self, title, author):
         self.title = title
         self.author = author
-        self.year = year
 
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.year})"
+    def _str_(self):
+        return f"{self.title} by {self.author}"
 
-    def __repr__(self):
-        return f"Book(title='{self.title}', author='{self.author}', year={self.year})"
+class EBook(Book):
+    def _init_(self, title, author, file_size):
+        super()._init_(title, author)
+        self.file_size = file_size
 
-    def __eq__(self, other):
-        if not isinstance(other, Book):
-            return NotImplemented
-        return (
-            self.title == other.title and
-            self.author == other.author and
-            self.year == other.year
-        )
+    def _str_(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}MB"
+
+class PrintBook(Book):
+    def _init_(self, title, author, pages):
+        super()._init_(title, author)
+        self.pages = pages
+
+    def _str_(self):
+        return f"PrintBook: {self.title} by {self.author}, Pages: {self.pages}"
