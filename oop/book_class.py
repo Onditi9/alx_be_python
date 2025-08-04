c8e7b6b14c2e92e812490a0a733ae8f39ab5ceac
Onditi9 <omwegajoseph01@gmail.com>
2025-08-05 00:06:37 +0300

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,20 +1,11 @@
 class Book:
-    def __init__(self, title, author, year):
+    def _init_(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.year})"
+    def _repr_(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    def __repr__(self):
-        return f"Book(title='{self.title}', author='{self.author}', year={self.year})"
-
-    def __eq__(self, other):
-        if not isinstance(other, Book):
-            return NotImplemented
-        return (
-            self.title == other.title and
-            self.author == other.author and
-            self.year == other.year
-        )
+    def _del_(self):
+        print(f"Book '{self.title}' is being deleted.")
