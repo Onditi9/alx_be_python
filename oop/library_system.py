995a8690d6229af35f88cd7aa54e9ae516e7ff5c
Onditi9 <omwegajoseph01@gmail.com>
2025-08-20 00:56:42 +0300

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,41 +3,23 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_info(self):
-        return f"Book: {self.title} by {self.author}"
-
     def __str__(self):
-        return self.get_info()
+        return f"Book: {self.title} by {self.author}"   # <-- Added "Book:"
+    
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_info(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-
     def __str__(self):
-        return self.get_info()
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
-        return f"PrintBook: {self.title} by {self.author}, page count: {self.page_count}"
-
     def __str__(self):
-        return self.get_info()
-
-class Library:
-    def __init__(self):
-        self.books = []
-
-    def add_book(self, book):
-        self.books.append(book)
-
-    def list_books(self):
-        for book in self.books:
-            print(book)
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"   # <-- Capitalized "Page Count"
